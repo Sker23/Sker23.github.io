4ca75b630e9ed1f1fd24082dd8c79a8f960482f9
Sker23 <noth07@bahnhof.se>
2025-05-19 15:26:13 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -1,13 +1,10 @@
-/**
- * Se detta som en grund att utgå ifrån.
- * Det är helt fritt att ändra och ta bort kod om ni
- * önskar lösa problemen med andra metoder.
- */
 
 let lcd = null; // displayen
 
 let memory = 0; // Lagrat/gamlat värdet från display
 let arithmetic = null; // Vilken beräkning som skall göras +,-, x eller /
+
+let isComma = false; //Om komma finns
 
 function init() {
     lcd = document.getElementById('lcd');
@@ -23,10 +20,18 @@ function buttonClick(e) {
 
     // kollar om siffertangent är nedtryckt
     if (btn.substring(0, 1) === 'b') {
-        let digit = btn.substring(1, 2); // plockar ut siffran från id:et
-
+     let digit = btn.substring(1, 2); // plockar ut siffran från id:et
+        addDigit(digit);
     } else { // Inte en siffertangent, övriga tangenter.
-
+        if (btn === 'comma') {
+            addComma();
+        } else if (btn === 'clear') {
+            memClear();
+        } else if (btn === 'enter') {
+            calculate();
+        } else {
+            setOperator(btn);
+        }
     }   
 }
 
@@ -34,13 +39,17 @@ function buttonClick(e) {
  *  Lägger till siffra på display.
  */
 function addDigit(digit) {
+    lcd.value += digit; //lägger till nästa nummber
 }
 
 /**
  * Lägger till decimaltecken
  */
 function addComma() {
-
+    if (isComma === false) {
+        lcd.value += '.';
+        isComma = true;
+    }
 }
 
 /**
@@ -48,6 +57,27 @@ function addComma() {
  * +, -, *, /
  */
 function setOperator(operator){
+    if (arithmetic === null) {
+        memory = lcd.value;
+        clearLCD();
+        isComma = false;
+    }
+
+    if (operator === 'add') {
+        arithmetic = '+';
+    }
+
+    if (operator === 'sub') {
+        arithmetic = '-';
+    }
+
+    if (operator === 'mul') {
+        arithmetic = '*';
+    }
+
+    if (operator === 'div') {
+        arithmetic = '/';
+    }
 
 }
 
@@ -55,7 +85,30 @@ function setOperator(operator){
  * Beräknar ovh visar resultatet på displayen.
  */
 function calculate() {
+    let x = Number(memory)
+    let y = Number(lcd.value)
 
+    if (arithmetic === '+') {
+        lcd.value = x + y;
+    }
+
+    if (arithmetic === '-') {
+        lcd.value = x - y;
+    }
+
+    if (arithmetic === '*') {
+        lcd.value = x * y;
+    }
+
+    if (arithmetic === '/') {
+        if (y === 0) {
+            lcd.value = 'ERROR CANT DIVIDE BY 0'
+        } else {
+            lcd.value = x / y;
+        }
+    }
+
+    arithmetic = null;
 }
 
 /** Rensar display */
